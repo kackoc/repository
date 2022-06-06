0d39de5a53a75424415c19b74503170eefd01ce6
kackoc <kapi159@onet.pl>
2022-06-06 11:15:52 +0200

    MSinit

---
+++ MSUnit/PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Prime.Services;
+
+namespace Prime.UnitTests.Services
+{
+    [TestClass]
+    public class PrimeService_IsPrimeShould
+    {
+        private readonly PrimeService _primeService;
+
+        public PrimeService_IsPrimeShould()
+        {
+            _primeService = new PrimeService();
+        }
+
+        // < 2
+        [TestMethod]
+        public void Test_False()
+        {
+            var result = _primeService.IsPrime(-1);
+
+            Assert.IsFalse(result, $"-1 should not be prime");
+        }
+
+        
+
+        // - < 2
+        [DataTestMethod]
+        [DataRow(-1)]
+        [DataRow(0)]
+        [DataRow(1)]
+        public void Theory1_False(int value)
+        {
+            var result = _primeService.IsPrime(value);
+
+            Assert.IsFalse(result, $"{value} should not be prime");
+        }
+
+        //- > 1
+        [DataTestMethod]
+        [DataRow(2)]
+        [DataRow(3)]
+        [DataRow(5)]
+        [DataRow(7)]
+        public void Theory2_False(int value)
+        {
+            var result = _primeService.IsPrime(value);
+
+            Assert.IsTrue(result, $"{value} should be prime");
+        }
+
+        [DataTestMethod]
+        [DataRow(4)]
+        [DataRow(6)]
+        [DataRow(8)]
+        [DataRow(9)]
+        public void Theory3_False(int value)
+        {
+            var result = _primeService.IsPrime(value);
+
+            Assert.IsFalse(result, $"{value} should not be prime");
+        }
+    }
+}
